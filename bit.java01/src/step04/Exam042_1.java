c7a902536c081fb34534e919b27833eb61683220
Jinyoung <jinyoung.eom@gmail.com>
2016-10-19 15:09:55 +0900

    .

---
+++ bit.java01/src/step04/Exam042_1.java
@@ -0,0 +1,32 @@
+// 주제: 연산자 - 비트 논리 연산자 응용 (상)
+// => AND 연산자의 활용 전
+
+package step04;
+
+public class Exam042_0 {
+  public static void main(String[] args) {
+    //문제: 이력서에 프로그래밍 가능한 항목을 입력 받기
+    // => 프로그래밍 보기
+    //    Java, JavaScript, HTML, CSS, C, C++, PHP, Python, Perl, VB,
+    //    R, Groovy, XML, Scala, Ruby, Swift, Object-C, Pascal, C#, Go
+    boolean[] language = new boolean[] {
+      true, true, true, true, true, true, true, true, true, true,
+      false, true, true, false, false, false, true, true, false, false
+    };
+
+    // Java, C, C++을 할 줄 아는 사람인지 검사하라!
+    
+  }
+}
+
+/*
+
+
+
+
+
+
+
+
+
+*/
