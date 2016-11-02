c8bede039fe775fae5b38d1f2b2c0ecac4ae911e
Jinyoung <jinyoung.eom@gmail.com>
2016-11-02 09:58:32 +0900

    .

--- bit.java01/src/step13/v2/BoxTest.java
+++ bit.java01/src/step13/v2/BoxTest.java
@@ -1,22 +1,67 @@
 /* 값을 저장할 때 사용할 메모리를 정의한다.*/
 package step13.v2;
+import java.util.Scanner;
 
 public class BoxTest  {
+  static Box head;
+  static Box tail;
+  static int length;
+  static Scanner keyScan;
+
+  static {
+    head = new Box();
+    tail = head;
+    length = 0;
+    keyScan = new Scanner(System.in);
+  }
+
   public static void main(String[] args) {
-    // 1) 상자를 생성하여 값을 보관한다.
-    Box b1 = new Box(100);
-    Box b2 = new Box(200);
-    Box b3 = new Box(300);
+    loop:
+    while (true) {
+      System.out.print("명령> ");
+      String command = keyScan.nextLine();
+      switch (command) {
+      case "add": doAdd(); break;
+      case "list": doList(); break;
+      case "get": doGet(); break;
+      case "quit": System.out.println("Good Bye~");
+        break loop;
+      default: System.out.println("명령어가 없습니다");
+      }
 
-    // 2) 이 상자들을 연결한다.
-    b1.next = b2;
-    b2.next = b3;
+    }
 
-    // 3) 첫 번째 상자부터 마지막 상자까지 값을 출력한다.
-    Box cursor = b1;
-    while (cursor != null) {
-      System.out.println(cursor);
+  }
+
+  static void doAdd() {
+    System.out.print("입력할 값? ");
+    int value = Integer.parseInt(keyScan.nextLine());
+    tail.value = value;
+    tail.next = new Box();
+    tail = tail.next;
+    length++;
+  }
+
+  static void doList() {
+    Box cursor = head;
+    while (cursor != null && cursor != tail) {
+      System.out.println(cursor.value);
       cursor = cursor.next;
     }
+  }
+
+  static void doGet() {
+    System.out.print("인덱스? ");
+    int index = Integer.parseInt(keyScan.nextLine());
+
+    if (index < 0 || index >= length) {
+      System.out.println("인덱스가 유효하지 않습니다.");
+      return;
+    }
+
+    Box cursor = head;
+    for (int i = 0; i < index; i++)
+      cursor = cursor.next;
+    System.out.println(cursor.value);
   }
 }
