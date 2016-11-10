a9fcdd81c883692fe26a3cda06df6f92a43a6054
eomjinyoung <jinyoung.eom@gmail.com>
2016-11-10 10:09:30 +0900

    .

---
+++ bit.java01/src/step18/Exam097_0.java
@@ -0,0 +1,27 @@
+/* 주제: 버퍼의 사용 - 버퍼 사용 전, 읽은 데 걸린 시간
+
+*/
+package step18;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Exam097_0 {
+  public static void main(String[] args) throws Exception {
+    FileInputStream in = new FileInputStream("temp/jls8.pdf");
+
+    //1) 읽기 전 시각 측정
+    //=> 1970년 1월 1일 0시 0분 0초부터 지금까지 경과된 밀리초
+    long startMillis = System.currentTimeMillis();
+
+    int count = 0;
+    while (in.read() != -1) count++;
+
+    //2) 읽은 후 시각 측정
+    long endMillis = System.currentTimeMillis();
+
+    System.out.printf("%d: %d\n", count, endMillis - startMillis);
+
+    in.close();
+  }
+}
---
+++ bit.java01/src/step18/Exam097_1.java
@@ -0,0 +1,28 @@
+/* 주제: 버퍼의 사용 - 버퍼 사용 후, 읽은 데 걸린 시간
+
+*/
+package step18;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Exam097_1 {
+  public static void main(String[] args) throws Exception {
+    FileInputStream in = new FileInputStream("temp/jls8.pdf");
+
+    //1) 읽기 전 시각 측정
+    //=> 1970년 1월 1일 0시 0분 0초부터 지금까지 경과된 밀리초
+    long startMillis = System.currentTimeMillis();
+
+    int count = 0;
+    byte[] buf = new byte[8192];
+    while (in.read(buf) != -1) count++;
+
+    //2) 읽은 후 시각 측정
+    long endMillis = System.currentTimeMillis();
+
+    System.out.printf("%d: %d\n", count, endMillis - startMillis);
+
+    in.close();
+  }
+}
