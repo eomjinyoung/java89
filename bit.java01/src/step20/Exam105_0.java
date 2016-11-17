e232b6c181ff73b3d4851c5c291063f19680b657
eomjinyoung <jinyoung.eom@gmail.com>
2016-11-17 16:13:39 +0900

    .

--- bit.java01/src/step20/Exam105_0.java
+++ bit.java01/src/step20/Exam105_0.java
@@ -7,6 +7,27 @@
  *              서버는 스스로 포트 번호를 지정한다.
  *              클라이언트는 OS로부터 자동으로 배정 받는다.
  *              포트번호는 중복되어서는 안된다.
+ * well-known port : 0 ~ 1023 까지의 포트번호를 말한다.
+ *                   기존에 유명 서비스가 주로 사용하는 번호로 일반 프로그램에서 사용하지 말도록
+ *                   권고하고 있다.
+ *                   20 - FTP 데이터 전송 포트
+ *                   21 - FTP 제어 포트 
+ *                   22 - SSH(Secure Shell) 서버 = Secure 텔렛 버전
+ *                   23 - 텔렛 서버
+ *                   25 - SMTP 서버. 메일 발송 서버
+ *                   53 - DNS 서버
+ *                   80 - 웹 서버가 사용
+ *                   109 - POP2 서버. 메일 받는 서버. 
+ *                   110 - POP3 서버. 메일 받는 서버.
+ *                   119 - 뉴스 서버
+ *                   143 - IMAP4 서버. 메일 받는 서버.
+ *                   443 - SSL 방식의 HTTP = HTTPS
+ *                   8080 - 프록시 서버 
+ *                   1521 - 오라클 DBMS 서버
+ *                   3306 - MySQL DBMS 서버
+ *                   
+ *                   
+ *                   
  * IP 주소:     각 컴퓨터에 부여된 인터넷 주소이다.
  *              IPv4에서는 0~255.0~255.0~255.0~255 이렇게 표시한다.
  *              4바이트 값을 이용하여 IP 주소를 표현한다.
@@ -47,8 +68,48 @@
  */
 package step20;
 
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Exam105_0 {
   public static void main(String[] args) throws Exception {
+    //서버 만들기
+    //1) 클라이언트와의 연결을 수행하는 객체 준비
+    // new ServerSocket(포트번호)
+    ServerSocket ss = new ServerSocket(8888); 
+    System.out.println("서버 실행중...");
+    
+    try (
+        //2) 대기열에서 기다리고 있는 클라이언트 중 한 개의 접속을 승인한다.
+        Socket socket = ss.accept();
+        
+        //3) 데이터를 주고 받기 위한 입출력 스트림 객체 준비
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        
+        //사용자로부터 입력 받기 위한 객체 준비
+        Scanner keyScan = new Scanner(System.in);) {
+      
+      System.out.println("클라이언트와 연결되었음");
+      
+      //4) 클라이언트가 보낸 데이터를 읽는다.
+      System.out.println(in.nextLine());
+      
+      //5) 클라이언트로 데이터를 보내기 전에 사용자의 입력을 기다린다.
+      
+      System.out.print("대답? ");
+      String message = keyScan.nextLine();
+      
+      //6) 사용자가 입력한 메시지를 클라이언트로 보낸다.
+      out.println(message);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    //7) 서버 소켓을 닫는다. => 더 이상 클라이언트와 연결할 수 없다.
+    ss.close();
     
   }
 }
---
+++ bit.java01/src/step20/Exam105_1.java
@@ -0,0 +1,47 @@
+/* 주제: 네트워킹 프로그래밍 - 클라이언트 소켓 
+ * => 서버와 연결할 때 사용하는 도구
+ */
+package step20;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Exam105_1 {
+  public static void main(String[] args) throws Exception {
+    try (
+        //소켓 만들기
+        //1) 서버와의 연결을 수행하는 객체 준비
+        // new Socket(서버주소, 포트번호)
+        // 서버주소: 도메인명, IP 주소. 로컬 도메인명 "localhost", 로컬 기본 IP "127.0.0.1"이다.
+        Socket socket = new Socket("127.0.0.1", 8888); 
+        
+        //2) 데이터를 주고 받기 위한 입출력 스트림 객체 준비
+        Scanner in = new Scanner(socket.getInputStream());
+        PrintStream out = new PrintStream(socket.getOutputStream());
+        
+        //사용자로부터 입력 받기 위한 객체 준비
+        Scanner keyScan = new Scanner(System.in);) {
+      
+      System.out.println("서버와 연결되었음");
+      
+      //3) 서버로 보낼 데이터를 사용자로부터 입력 받는다.
+      System.out.print("보낼 메시지? ");
+      String message = keyScan.nextLine();
+      
+      //4) 서버에 데이터를 보낸다.
+      out.println(message);
+      
+      //5) 서버가 보낸 데이터를 읽고 출력한다.
+      System.out.println(in.nextLine());
+     
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+
+
+
+
