391762cdb38ff3c040ab133abdaad068facf7470
eomjinyoung <jinyoung.eom@gmail.com>
2016-11-30 16:12:44 +0900

    .

--- bit.java01/src/step25/Test01.java
+++ bit.java01/src/step25/Test01.java
@@ -1,9 +1,22 @@
 package step25;
 
-import java.net.URL;
+import java.util.ArrayList;
 
 public class Test01 {
   public static void main(String[] args) throws Exception {
     
+    m();
+    System.out.println("---------------");
+    m("홍길동", "임꺽정", "유관순");
+    System.out.println("---------------");
+    m(new String[]{"홍길동", "임꺽정", "유관순"});
+    System.out.println("---------------");
+    
+  }
+  
+  static void m(String... names) {
+    for (String name : names) {
+      System.out.println(name);
+    }
   }
 }
