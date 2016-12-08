1d4ba9bfe85591c3c135c0f8591592dabdb04fd9
eomjinyoung <jinyoung.eom@gmail.com>
2016-12-08 12:56:40 +0900

    .

--- bit.web01/src/main/java/bitcamp/servlet2/Servlet15_view.java
+++ bit.web01/src/main/java/bitcamp/servlet2/Servlet15_view.java
@@ -15,8 +15,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet("/servlet15")
-public class Servlet15 extends GenericServlet {
+@WebServlet("/servlet15/view")
+public class Servlet15_view extends GenericServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
@@ -32,30 +32,34 @@ public class Servlet15 extends GenericServlet {
     try {
       Class.forName("com.mysql.jdbc.Driver");
       con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java89db", "java89", "1111");
-      stmt = con.prepareStatement("select uid, pwd, name, email from ex_students");
+      stmt = con.prepareStatement(
+          "select uid, pwd, name, tel, email, work, byear, schl "
+          + " from ex_students"
+          + " where uid=?");
+      stmt.setString(1, request.getParameter("userId"));
       rs = stmt.executeQuery(); 
       
       out.println("<!DOCTYPE html>");
       out.println("<html>");
       out.println("<head>");
       out.println("<meta charset='UTF-8'>");
-      out.println("<title>학생관리-목록</title>");
+      out.println("<title>학생관리-상세정보</title>");
       out.println("</head>");
       out.println("<body>");
       out.println("<h1>학생 정보</h1>");
       out.println("<table border='1'>");
-      out.println("<tr>");
-      out.println("  <th>아이디</th><th>암호</th><th>이름</th><th>이메일</th>");
-      out.println("</tr>");
       
-      while (rs.next()) { // 서버에서 레코드 한 개를 가져왔다면,
-        out.println("<tr> ");
-        out.printf("  <td>%s</td><td>%s</td><td>%s</td><td>%s</td>\n",
-          rs.getString("uid"),
-          rs.getString("pwd"),
-          rs.getString("name"),
-          rs.getString("email"));
-        out.println("</tr>");
+      if (rs.next()) { // 서버에서 레코드 한 개를 가져왔다면,
+        out.printf("<tr><th>아이디</th><td>%s</td></tr>\n", rs.getString("uid"));
+        out.printf("<tr><th>암호</th><td>%s</td></tr>\n", rs.getString("pwd"));
+        out.printf("<tr><th>이름</th><td>%s</td></tr>\n", rs.getString("name"));
+        out.printf("<tr><th>이메일</th><td>%s</td></tr>\n", rs.getString("email"));
+        out.printf("<tr><th>전화</th><td>%s</td></tr>\n", rs.getString("tel"));
+        out.printf("<tr><th>재직여부</th><td>%s</td></tr>\n", rs.getString("work"));
+        out.printf("<tr><th>태어난해</th><td>%d</td></tr>\n", rs.getString("byear"));
+        out.printf("<tr><th>최종학교</th><td>%s</td></tr>\n", rs.getString("schl"));
+      } else {
+        out.println("<tr><td>해당 사용자가 없습니다.</td></tr>");
       }
       
       out.println("</table>");
