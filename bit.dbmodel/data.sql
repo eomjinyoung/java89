55bd31f07d2b9bee553c7ae69076ed7209c9fe60
eomjinyoung <jinyoung.eom@gmail.com>
2016-12-16 10:19:59 +0900

    .

---
+++ bit.dbmodel/data.sql
@@ -0,0 +1,54 @@
+-- 회원 데이터
+insert into memb(mno,email,pwd,name,tel) values(1,'user01@test.com',password('1111'),'학생1','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(2,'user02@test.com',password('1111'),'학생2','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(3,'user03@test.com',password('1111'),'학생3','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(4,'user04@test.com',password('1111'),'학생4','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(5,'user05@test.com',password('1111'),'학생5','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(6,'user06@test.com',password('1111'),'학생6','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(7,'user07@test.com',password('1111'),'학생7','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(8,'user08@test.com',password('1111'),'학생8','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(9,'user09@test.com',password('1111'),'학생9','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(10,'user10@test.com',password('1111'),'학생10','1111-1111');
+
+insert into memb(mno,email,pwd,name,tel) values(21,'user21@test.com',password('1111'),'강사1','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(22,'user22@test.com',password('1111'),'강사2','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(23,'user23@test.com',password('1111'),'강사3','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(24,'user24@test.com',password('1111'),'강사4','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(25,'user25@test.com',password('1111'),'강사5','1111-1111');
+
+insert into memb(mno,email,pwd,name,tel) values(31,'user31@test.com',password('1111'),'매니저1','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(32,'user32@test.com',password('1111'),'매니저2','1111-1111');
+insert into memb(mno,email,pwd,name,tel) values(33,'user33@test.com',password('1111'),'매니저3','1111-1111');
+
+-- 학생 데이터
+insert into stud(sno,work,lst_schl,schl_nm) values(1,'Y','학사','비트대학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(2,'Y','학사','한국대학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(3,'Y','학사','하버드대학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(4,'N','고졸','비트고등학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(5,'N','고졸','성남고등학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(6,'Y','고졸','부산고등학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(7,'Y','학사',null);
+insert into stud(sno,work,lst_schl,schl_nm) values(8,'N','학사',null);
+insert into stud(sno,work,lst_schl,schl_nm) values(9,'Y',null,'비트고등학교');
+insert into stud(sno,work,lst_schl,schl_nm) values(10,'N',null,'성남고등학교');
+
+-- 강사 데이터
+insert into tcher(tno,hmpg,fcbk,twit) values(21,'http://www.user21.com',null,null);
+insert into tcher(tno,hmpg,fcbk,twit) values(22,null,'www.facebook.com/user22',null);
+insert into tcher(tno,hmpg,fcbk,twit) values(23,null,'www.facebook.com/user23','www.twitter.com/user23');
+insert into tcher(tno,hmpg,fcbk,twit) values(24,null,null,'www.twitter.com/user24');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
