ee77737b860a090bf8795a36f264ca6e57bb2a7b
eomjinyoung <jinyoung.eom@gmail.com>
2016-12-26 13:05:22 +0900

    .

--- bit.web01/src/main/java/bitcamp/servlet10/Servlet41.java
+++ bit.web01/src/main/java/bitcamp/servlet10/Servlet41.java
@@ -52,14 +52,21 @@ Content-Type: image/jpeg
  */
 package bitcamp.servlet10;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 @WebServlet("/servlet/servlet41")
 public class Servlet41 extends HttpServlet {
@@ -69,15 +76,56 @@ public class Servlet41 extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
       throws ServletException, IOException {
     
-    request.setCharacterEncoding("UTF-8");
+    // 멀티파트 형식에는 다음 문자집합 처리가 소용없다.
+    //request.setCharacterEncoding("UTF-8");
+    
+    // 멀티파트 형식 데이터인지 여부 확인
+    if (!ServletFileUpload.isMultipartContent(request)) {
+      throw new ServletException("멀티파트 형식이 아닙니다.");
+    }
+    
+    // 클라이언트가 보낸 데이터를 저장할 객체 준비
+    // => HDD 에 저장할 거라면, 다음 객체를 생성하라!
+    DiskFileItemFactory itemFactory = new DiskFileItemFactory();
+    
+    // 멀티파트 데이터를 분석 처리할 객체 준비
+    // => 이때 생성자 파라미터 값으로 데이터 저장을 처리하는 공장을 넘겨야 한다.
+    ServletFileUpload uploadHandler = new ServletFileUpload(itemFactory);
     
     response.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = response.getWriter();
+
+    // 요청 데이터의 분석을 실행한다.
+    try {
+      // 리턴값: 멀티파트를 분석하여 그 결과를 목록으로 리턴한다.
+      List<FileItem> items = uploadHandler.parseRequest(request);
+      
+      for (FileItem item : items) {
+        if (item.isFormField()) { // 일반 폼 항목이면,
+          out.printf("%s=%s\n", item.getFieldName(), item.getString("UTF-8"));
+        } else { // 파일이면,
+          out.printf("%s=%s\n", item.getFieldName(), item.getName());
+          
+          // 임시 저장소에 있는 파일을 지정된 폴더로 옮긴다.
+          // => 주의! 프로젝트 폴더에 업로하는 것이 아니다.
+          /*
+          item.write(new File(
+              "/Users/eomjinyoung/git/java89/bit.web01/src/main/webapp/upload/" 
+              + item.getName()));
+          */
+          // => 웹 애플리케이션이 배치된 폴더에 두어야 한다.
+          ServletContext sc = this.getServletContext();
+          out.printf("실제저장될 경로=%s\n", sc.getRealPath("/upload/" + item.getName()));
+          item.write(new File(
+              // ServletContext는 "/upload/파일명"의 실제 배치 경로를 알아준다.
+              sc.getRealPath("/upload/" + item.getName())));
+        }
+      }
+      
+    } catch (Exception e) {
+      throw new ServletException(e);
+    }
     
-    out.printf("userId=%s\n", request.getParameter("userId"));
-    out.printf("password=%s\n", request.getParameter("password"));
-    out.printf("name=%s\n", request.getParameter("name"));
-    out.printf("photo=%s\n", request.getParameter("photo"));
   }
   
 }
