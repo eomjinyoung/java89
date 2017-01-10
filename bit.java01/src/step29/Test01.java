b92be6aeaa3af0a4cb133565cea8a6e2b976ec98
eomjinyoung <jinyoung.eom@gmail.com>
2017-01-10 10:23:13 +0900

    .

---
+++ bit.java01/src/step29/Test01.java
@@ -0,0 +1,26 @@
+/* 주제: Mybatis 설정과 실행
+ * 1) Mybatis 라이브러리 추가
+ *   - build.gradle 파일에 의존 라이브러리 정보 추가
+ *   - "gradle eclipse" 명령 실행
+ *   - 프로젝트 "refresh" 실행
+ *   - 프로젝트에 라이브러리가 등록되었는지 확인 
+ * 2) Mybatis 설정 파일 준비
+ *   - mybatis-config.xml 파일 생성
+ *   - 설정 정보 작성
+ * 3) DBMS 연결 정보를 담은 프로퍼티 파일 준비
+ *   - jdbc.properties 파일 생성
+ * 3) SQL 맵퍼 파일 준비 
+ *   - SQL을 저장한 파일
+ *   - 보통 테이블 당 한 개의 맵퍼 파일 생성
+ *   - ContactsMapper.xml
+ */
+package step29;
+
+public class Test01 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
